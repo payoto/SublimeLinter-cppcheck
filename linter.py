24e83674ced9f0c209da62fa30eb3d68a74d9c8e
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2018-07-20 12:05:17 +0100

    Add error suppression option
    Adds the ability to suppress individual error messages by
    using the "--suppress " option of cppcheck

--- linter.py
+++ linter.py
@@ -15,6 +15,7 @@ class Cppcheck(Linter):
         'selector': 'source.c, source.c++',
         '--std=,+': [],  # example ['c99', 'c89']
         '--enable=,': 'style',
+        '--supress=,+':[], # example ["missingOverride"]
     }
 
     def split_match(self, match):
